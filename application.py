3c51c50a92922db2d40b1cafaa84777b39193f0a
avinashlone22 <avinashlone22@gmail.com>
2024-12-08 15:29:05 +0000

    changes final 1

--- application.py
+++ application.py
@@ -286,4 +286,4 @@ if __name__ == '__main__':
     with application.app_context():
         db.create_all()
     application.run(debug=True, host='0.0.0.0', port=8080)
-#jfhdsjfhhuifcyniuenfgy
+#jfhdsjfhhuifcyniuenfgyfhbrhfejnjejfnjewnfjnewjfjwefekkk
